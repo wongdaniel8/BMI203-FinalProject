6e7649ebfbf1f49f6f535c61b4e10c707fbd0f78
UCSF Student <student@otp589625ots.ucsf.edu>
2018-03-14 12:51:09 -0700

    ready for submission

--- neuralNet.py
+++ neuralNet.py
@@ -8,18 +8,21 @@ import sys
 from matplotlib import pyplot as plt
 
 testSet = []
+full = []
 
-def convertLabel(lab):
+def convertLabel(lab, arraySize):
     """
-    converts a label (0 or 1) to a vector of length 2, with a 1 being in index {label}
+    converts a label (0 or 1) to a vector of length arraySize, with a 1 being in index {label}
     """
-    ar = np.zeros(2)
+    lab = int(lab)
+    ar = np.zeros(arraySize)
     ar[lab] = 1
     return ar
 
 #preprocessing 
 def getData(split=True):
     global testSet
+    global full
     #get raw data into list of sequences
     positives = []
     with open("rap1-lieb-positives.txt") as f:
@@ -102,12 +105,12 @@ def getData(split=True):
     y = []
     for el in encodedPos:
         X.append(el)
-        y.append(convertLabel(1))
+        y.append(convertLabel(1, 2))
 
     randomNegs = random.sample(encodedNeg, 137) #training will be 274 total sequences (half positive half negative), negatives will be chosen randomly
     for el in randomNegs:
         X.append(el)
-        y.append(convertLabel(0))
+        y.append(convertLabel(0, 2))
 
     X = np.array(X, np.int32)
     y = np.array(y)
@@ -117,8 +120,9 @@ def getData(split=True):
     xFict[:,:-1] = X
     X = np.copy(xFict)
 
-    # X = preprocessing.normalize(X, norm='l2')
+    full = X
 
+    # X = preprocessing.normalize(X, norm='l2')
     
     if split == False:
         return X, y
@@ -133,7 +137,7 @@ class Neural_Network(object):
         #Define Hyperparameters
         self.inputLayerSize = 1000 + 1
         self.outputLayerSize = 2
-        self.hiddenLayerSize = 200 + 1
+        self.hiddenLayerSize = 100 + 1
 
         #define layers and activations
         self.x = [] #input layer
@@ -141,10 +145,10 @@ class Neural_Network(object):
         self.z = [] #post sigmoid function values in output layer
         self.u = [] #pre-tanh function values in hidden layer
         self.uPrime = [] #pre-sigmoid function values in ouput layer
-
+        self.epsilon = .1
         #Weights (parameters)
-        self.V = np.random.normal(0, .01, (1001, self.hiddenLayerSize - 1))
-        self.W = np.random.normal(0, .01, (self.hiddenLayerSize, 2))
+        self.V = np.random.normal(0, .01, (self.inputLayerSize, self.hiddenLayerSize - 1))
+        self.W = np.random.normal(0, .01, (self.hiddenLayerSize, self.outputLayerSize))
 
     def sigmoid(self, z):
     #Apply sigmoid activation function to scalar, vector, or matrix
@@ -170,7 +174,7 @@ class Neural_Network(object):
         
     def forward(self, X, batch):
         """
-        batch will be true if we pass in a batch of images for X to compute y hat
+        batch will be true if we pass in a batch of sequences for X to compute y hat
         """
         #Propagate inputs though network
         self.u = np.dot(X, self.V)
@@ -183,9 +187,7 @@ class Neural_Network(object):
             self.uPrime = np.dot(hFict, self.W)
         else:
             self.uPrime = np.dot(np.append(hcopy, [1]), self.W)
-        # self.uPrime = np.dot(self.h, self.W)
         yHat = self.sigmoid(self.uPrime) 
-        # print(yHat.shape, yHat)
         return yHat
 
     def costFunction(self, X, y):
@@ -199,15 +201,13 @@ class Neural_Network(object):
         Compute derivative with respect to V and W for a given sample X and label y
         implement batch functionality later for batch gradient descent
         """
-        # dJdW = 201x2 # dJdV = 1001x201  # delta2 = 1x201 but want 1x200???
-         # dJdV = want 1001x200 
         if batch == False:
             self.yHat = self.forward(X, batch)
             delta3 = np.multiply(- (y - self.yHat), self.sigmoidPrime(self.uPrime))
-            dJdW = np.dot(np.reshape(np.append(self.h, [1]), (1, self.hiddenLayerSize)).T, np.reshape(delta3, (1, 2)))
+            dJdW = np.dot(np.reshape(np.append(self.h, [1]), (1, self.hiddenLayerSize)).T, np.reshape(delta3, (1, self.outputLayerSize)))
 
             delta2 = np.dot(delta3, np.delete(self.W.T, np.s_[-1:], 1)) * (1 - (self.tanh(self.h))**2)####self.h does not have fict dimension
-            dJdV = np.dot(np.reshape(X, (1, 1001)).T, np.reshape(delta2, (1, self.hiddenLayerSize - 1)))####
+            dJdV = np.dot(np.reshape(X, (1, self.inputLayerSize)).T, np.reshape(delta2, (1, self.hiddenLayerSize - 1)))####
             return dJdV, dJdW #dV is mostly 0s
 
     def crossEntropyCost(self, X, y):
@@ -218,20 +218,19 @@ class Neural_Network(object):
     def crossEntropyCostPrime(self, X, y, batch):
         self.yHat = self.forward(X, batch)
         delta3 = -1 * np.multiply((y/self.yHat) - ((1-y)/(1-self.yHat)), self.sigmoidPrime(self.uPrime))
-        dJdW = np.dot(np.reshape(np.append(self.h, [1]), (1, self.hiddenLayerSize)).T, np.reshape(delta3, (1, 2)))
+        dJdW = np.dot(np.reshape(np.append(self.h, [1]), (1, self.hiddenLayerSize)).T, np.reshape(delta3, (1, self.outputLayerSize)))
         delta2 = np.dot(delta3, np.delete(self.W.T, np.s_[-1:], 1)) * (1 - (self.tanh(self.h))**2)####self.h does not have fict dimension
-        dJdV = np.dot(np.reshape(X, (1, 1001)).T, np.reshape(delta2, (1, self.hiddenLayerSize - 1)))####
+        dJdV = np.dot(np.reshape(X, (1, self.inputLayerSize)).T, np.reshape(delta2, (1, self.hiddenLayerSize - 1)))####
         return dJdV, dJdW #dV is mostly 0s
 
 
-    def trainNeuralNetwork(self, images, labels, validation, validationLabels, costType,verbose=True):
+    def trainNeuralNetwork(self, sequences, labels, validation, validationLabels, costType,verbose=True):
         # print("V: ", self.V)
         # print("W: ", self.W)
-        # print("lengths: ", len(images), len(labels), len(validation))
+        # print("lengths: ", len(sequences), len(labels), len(validation))
         errorRate = 1
         DONE = False
         iters = 0
-        epsilon =  0.1
         newThresh = .20
         trainingErrors = []
         itersList = []
@@ -240,27 +239,22 @@ class Neural_Network(object):
         cardinality = 274
         while iters < cardinality * 50: 
             #STOCHASTIC SINGLE sequence
-            sample = images[iters % len(images)] 
-            y = labels[iters % len(images)] 
+            sample = sequences[iters % len(sequences)] 
+            y = labels[iters % len(sequences)]
             if costType == "squared":
                 dv, dw = self.costFunctionPrime(sample, y, False) 
             else:
                 dv, dw = self.crossEntropyCostPrime(sample, y, False)
-            self.V = self.V - float(epsilon) * dv #subtract every element in vector v by dv
-            self.W = self.W - float(epsilon) * dw #subtract every element in vector w by dw
+            self.V = self.V - float(self.epsilon) * dv #subtract every element in vector v by dv
+            self.W = self.W - float(self.epsilon) * dw #subtract every element in vector w by dw
             iters += 1
-            
-            # if DONE == False and errorRate < .20:
-            #     epsilon = epsilon * .6
-            #     DONE = True
+           
             if errorRate < newThresh:
-                epsilon = epsilon * .6
+                self.epsilon = self.epsilon * .6
                 newThresh -= .05
                 DONE = True 
 
-           
             if iters % 10 == 0:
-            # if iters % cardinality == 0:
                 preds = self.predict("", validation)
                 mistakes = 0
                 for i in range(0, len(preds)):
@@ -269,67 +263,49 @@ class Neural_Network(object):
                 errorRate = mistakes / float(len(preds))
                 if verbose == True:
                     print(preds)
-                    print("iteration: ", iters, "epsilon: ", epsilon, " val error rate: ", mistakes / float(len(preds)))
+                    print("iteration: ", iters, "self.epsilon: ", self.epsilon, " val error rate: ", mistakes / float(len(preds)))
                 validationAccuracies.append(1 - errorRate)
             
-            if errorRate < .036:
-                print("less than .036 validation error")
-                avg = crossValidate(self, np.concatenate((images, validation)), np.concatenate((labels, validationLabels)), verbose=False)
-                if avg < .088:
+            if errorRate < .04:
+                print("less than .04 validation error")
+                avg = crossValidate(self, np.concatenate((sequences, validation)), np.concatenate((labels, validationLabels)), verbose=False)
+                if avg < .047:
                     break
 
-
-            # if iters % 10 == 0:
-            #     if costType == "squared":
-            #         cost = self.costFunction(images, labels)
-            #         costs.append(cost)
-            #     else:
-            #         cost = self.crossEntropyCost(images, labels) #PASSING IN ALL IMAGES TO COMPUTE COST
-            #         costs.append(cost)
-            #     if verbose == True:
-                    # print("COST", cost) 
-
-
-            #resample training data to include different negatives each epoch
-            # if iters % cardinality == 0:
-            #     images, null, labels, null = getData(True)
-            
-            # if iters % (cardinality/2) == 0:
-            #     print("epsilon", epsilon)
-            #     avg = crossValidate(self, np.concatenate((images, validation)), np.concatenate((labels, validationLabels)), verbose=False)
-            #     if avg < .067:
-            #         break
+            if iters % 30 == 0:
+                if costType == "squared":
+                    cost = self.costFunction(sequences,labels)
+                    costs.append(cost)
+                else:
+                    cost = self.crossEntropyCost(sequences, labels) 
+                    costs.append(cost)
+                if verbose == True:
+                    print("COST", cost) 
 
         return self.V, self.W
 
-    def predict(self, weights, images, getProbTrue=False):
+    def predict(self, weights, sequences, getProbTrue=False):
         """
         returns predictions as integers (binary classification) if getProbTrue = False
         else will return predictions as a list of probabilities of being a 1 (positive result)
         """
         predictions = []
-        # counts = 0
-        for image in images:
-            pred = self.forward(image, False)
-            # print(pred,1 - float(pred[1]) , 1 - float(pred[1]) > pred[0], 1 - float(pred[1]) - pred[0])
-            # if (1 - float(pred[1]) > pred[0]):
-            #     counts += 1
+        for seq in sequences:
+            pred = self.forward(seq, False)
             if getProbTrue:
                 # predictions.append(pred[1]) 
                 predictions.append(1 - pred[0]) #prob of not false = prob of being True (being positive), chosen this way because margin on left is slightly smaller
             else:
-                predictions.append(np.argmax(pred)) #arg min? 
-        # print("COUNTS", counts/float(len(predictions)))
+                predictions.append(np.argmax(pred)) 
         return predictions
 
 def getNN(costType):
     training, validation, trainingLabels, validationLabels = getData(True)
-    print(training.shape, validation.shape)
     X = np.concatenate((training, validation),axis=0)
     y = np.concatenate((trainingLabels, validationLabels))
     if costType == "squared":
         NN = Neural_Network()
-        NN.trainNeuralNetwork(training, trainingLabels, validation, validationLabels, "squared", verbose=True)
+        NN.trainNeuralNetwork(training, trainingLabels, validation, validationLabels, "squared", verbose=False)
     else:
         NN = Neural_Network()
         NN.trainNeuralNetwork(training, trainingLabels, validation, validationLabels, "entropy", verbose=False)
@@ -349,7 +325,6 @@ def crossValidate(NN, X, y, verbose=False):
     performs cross validation with an input neural network, prints results
     """
     folds = []
-    # X, y = getData(split=False)
     kf = model_selection.KFold(n_splits=10, random_state=None, shuffle=True)
     for train_index, test_index in kf.split(X):
         X_train, X_test = X[train_index], X[test_index]
@@ -359,6 +334,7 @@ def crossValidate(NN, X, y, verbose=False):
         for i in range(0, len(predictions)):
             if predictions[i] != np.argmax(y_test[i]):
                 mistakes += 1
+        print("iter k fold error", mistakes / float(len(predictions)))
         folds.append(mistakes / float(len(predictions)))
     print("average k fold", np.average(folds))
     return np.average(folds)
@@ -369,30 +345,51 @@ def predictOnTestSet():
     """
     global testSet
     NN = getNN("squared")
+    print("test on all ordered")
+    print(NN.predict("", full))
     testPredictions = NN.predict("", testSet, getProbTrue=True)
-    with open("predictions.txt", "w") as f:
+    with open("predictions_new3.txt", "w") as f:
         with open("rap1-lieb-test.txt", "r") as t:
             i = 0
             for line in t.readlines():
                 f.write(line.rstrip('\n') + '\t' + str(testPredictions[i]) + '\n')
                 i+=1
 
-NN = getNN("squared")
+def autoencoder():
+    """
+    check correctness of neural network by feeding it an identity matrix of length 8, 
+    see if it learns binary
+    """
+    NN = Neural_Network()
+    NN.inputLayerSize = 8 
+    NN.outputLayerSize = 8
+    NN.hiddenLayerSize = 3
+    NN.V = np.random.normal(0, .01, (NN.inputLayerSize, NN.hiddenLayerSize - 1))
+    NN.W = np.random.normal(0, .01, (NN.hiddenLayerSize, NN.outputLayerSize))
+    NN.epsilon = 5
+    print(NN.V.shape, NN.W.shape)
+    print(NN.inputLayerSize)
+    X = np.identity(8) 
+    y = X
+    training, validation, trainingLabels, validationLabels = model_selection.train_test_split(X, y, test_size= 2, random_state=42)
+    NN.trainNeuralNetwork(X, y, validation, validationLabels, "squared", verbose=True)
+    predictions = NN.predict("", X)
+    print("predictions (should be ordered 0->7):",predictions)
 
+
+# autoencoder()
+# NN = getNN("squared")
 # crossValidate()
 # predictOnTestSet()
 
+
+
+
+
+#========================================================================
 # Notes:
 # 274 total size of set (training + validation test)
-# with validation/test set size of 10%, 1 seq error = .0357 error rate
-
-# predictions file rn:
-# final error rate on validation:  0.03571428571428571
-# final k fold
-# average k fold 0.07711640211640211
-
-
-
+# predicitons_new2 has 0 error validation,  .04 k fold error 
 
 
 
